3efcfdf42dd04bf9919544ca2ee6bb9141dc2531
Bartosz Śnieżek <Bartosz.Sniezek@outlook.com>
2018-01-13 16:48:59 +0100

    zmiany

---
+++ src/main/resources/static/script/inwentaryzacja.js
@@ -0,0 +1,66 @@
+$(function () {
+
+
+    /*var ViewModel = function() {
+     var self = this;
+      self.items = ko.observableArray([{comment:'first comment', amount:0}]);
+      self.addNewItem = function(){
+        self.items.push(new Item('',0));
+      };
+ }
+  
+ var Item = function(comment, amount) {
+  var self = this;
+  self.comment = ko.observable(comment);
+  self.amount = ko.observable(amount);   
+  }; */
+
+
+    var Item = function (code, name, unit, quantity, unitprice, value, comments) {
+        var self = this;
+
+        self.code = ko.observable(code);
+        self.name = ko.observable(name);
+        self.unit = ko.observable(unit);
+        self.quantity = ko.observable(quantity);
+        self.unitprice = ko.observable(unitprice);
+        self.value = ko.observable(value);
+        self.comments = ko.observable(comments);
+    }
+
+    var ViewModel = function () {
+        var self = this;
+        self.dateIn = ko.observable('');
+        self.madeBy = ko.observable('');
+        self.ItemToAdd = ko.observable(new Item());
+        self.Items = ko.observableArray([]);
+
+        self.addItem = function (item) {
+            self.Items.push(item);
+            self.ItemToAdd(new Item());
+            $("#addButton").click(function () {
+                $('html, body').animate({
+                    scrollTop: $("#addButton").offset().bottom
+                }, 100);
+            });
+            toastr.info("dziala");
+        }
+
+        self.removeItem = function () {
+            self.Items.remove(this);
+        }
+
+    }
+
+    var ms = new ViewModel();
+
+    ko.applyBindings(ms);
+ /*
+vm = new ViewModel();
+ ko.applyBindings(vm);*/
+    $("#sendButton").click(function () {
+        console.log(ko.toJSON(ms));
+    });
+
+});
+
